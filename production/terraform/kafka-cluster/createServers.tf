d531a568f26fd33cd195f2bde61074ea9c51f088
johnzac <zacharia.john92@gmail.com>
2017-10-19 02:23:48 +0530

    Comit after fixing tag-names and private key names

--- production/terraform/kafka-cluster/createServers.tf
+++ production/terraform/kafka-cluster/createServers.tf
@@ -4,10 +4,10 @@ ami = "ami-9fa761e7"
 instance_type = "t2.micro"
 count = "3"
 vpc_security_group_ids = ["${aws_security_group.public-sg.id}"]
-key_name = "mysql-key"
+key_name = "kafka-key"
 subnet_id="${aws_subnet.public_subnet-us-west-2b.id}"
 user_data = "${file("userDataKafka")}"
 tags {
-    Name = "termnews-test"
+    Name = "kafka-prod"
   }
 }
